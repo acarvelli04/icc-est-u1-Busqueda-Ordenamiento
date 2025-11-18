76878a20ae21d2fde5393ecafaf1db510bee53b5
Isabel Ullauri <iullaurib@est.ups.edu.ec>
2025-11-18 08:50:04 -0500

    avanzado en clases

---
+++ src/views/View.java
@@ -0,0 +1,34 @@
+package views;
+import java.util.Scanner;
+import controllers.*;
+import models.Person;
+
+public class View {
+    private Scanner scanner = new Scanner(System.in);
+
+    public int showMenu(){
+        return i;
+    }
+
+    public Person inputPerson(){
+
+    }
+
+    public int selectSortingMethod(){
+
+    }
+
+    public int selectSearchCriterion(){
+
+    }
+
+    public void displayPersons(Person[]){
+
+    }
+
+    public void displayPersons(Person[]){
+
+    }
+
+
+}
